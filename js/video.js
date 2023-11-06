5d4d8ff201731e08ab27b102b401012e876e6d24
clairenat <claireca@umich.edu>
2023-11-06 11:26:11 -0500

    trying

--- js/video.js
+++ js/video.js
@@ -2,10 +2,10 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video.autoplay;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play();
+ });
